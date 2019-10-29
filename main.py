55688a801d5b197c9170fbb67c77df9cf6124495
GuillaumeM92 <56165265+GuillaumeM92@users.noreply.github.com>
2019-10-29 17:48:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,6 @@ from display import *
 from labyrinth_square_grid import *
 import random
 
-# Enable key holding
-pygame.key.set_repeat(delay, interval)
-
-# Define variables
-running = 1
-syringe_check = 1
-ether_check = 0
-sunglasses_check = 0
-
-draw_ether = 1
-draw_sunglasses = 1
-draw_syringe = 1
-
-picked_up_ether = 0
-picked_up_sunglasses = 0
-picked_up_syringe = 0
-
-picked_up_ether_and_syringe = 0
-crafted_ether_syringe = 0
-
-j = 0
-k = 0
-you_win = 0
-you_lose = 0
-
 # Game main loop
 while running:
 
